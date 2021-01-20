63477538afa354b9fbc3e546f5d67f6e861ce20e
alexandra2103 <pinheiro.alexandra21@gmail.com>
2021-01-20 15:48:37 +0100

    mise en place du dossier

---
+++ lib/dark_trader.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+
+page = Nokogiri::HTML(open("https://coinmarketcap.com/currencies/bitcoin/"))
+
+all_cryptos_links[]
+all_cryptos_links = page.xpath('/mettre_ici_le_XPath')
+
+
+def crypto_scrapper
+
+end
